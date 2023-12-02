4749d1259b6c50a07267ecf780409f4471915d18
Jester <58484964+QubitTooLate@users.noreply.github.com>
2023-12-02 17:25:00 +0100

    My first solution for day 2 part 2

--- samples/Console/Program.cs
+++ samples/Console/Program.cs
@@ -43,6 +43,18 @@ RunSolution<Day02.PartOneSolution02, string[]>(
 	File.ReadAllLines(GetPathForInput(2)),
 	2406);
 
+RunSolution<Day02.PartTwoSolution01, string[]>(
+	"""
+	Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+	Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+	Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+	Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+	Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+	""".Split('\n'),
+	2286,
+	File.ReadAllLines(GetPathForInput(2)),
+	78375);
+
 return;
 
 static void RunSolution<T, TInput>(TInput exampleInput, long expectedExampleResult, TInput input, long? knownResult = null) where T : ISolution<TInput>, new()
---
+++ src/Day02/PartTwoSolution01.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Qtl.AoC2023.Day02;
+
+public sealed partial class PartTwoSolution01 : ISolution<string[]>
+{
+	public long Solve(string[] input) =>
+		input
+			.Select(item =>
+				CountAndColor()
+					.Matches(item)
+					.Select(match => match.Value)
+					.Select(countColor => countColor.Split(' '))
+					.GroupBy(countColor => countColor[1])
+						.Select(countColorGroup => countColorGroup.MaxBy(countColor => int.Parse(countColor[0]))!)
+						.Select(countColor => int.Parse(countColor[0]))
+					.Aggregate((previous, current) => previous * current))
+			.Sum();
+
+	[GeneratedRegex("\\d+ (red|green|blue)")]
+	private static partial Regex CountAndColor();
+}
